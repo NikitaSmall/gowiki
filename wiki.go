0485edbdeec15f54c125417a7e8dfe7c08c234a7
nikitasmall <nikitasosnov@yahoo.com>
2015-08-25 11:29:05 +0300

    basic editing page implemented

--- wiki.go
+++ wiki.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// Page is our wiki page
 type Page struct {
 	Title string
 	Body  []byte
@@ -15,6 +16,25 @@ func servePage(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Body)
+}
+
+func editPage(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func savePage(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func (p *Page) save() error {
@@ -34,5 +54,7 @@ func loadPage(title string) (*Page, error) {
 
 func main() {
 	http.HandleFunc("/view/", servePage)
+	http.HandleFunc("/edit/", editPage)
+	http.HandleFunc("/save/", savePage)
 	http.ListenAndServe(":3000", nil)
 }
